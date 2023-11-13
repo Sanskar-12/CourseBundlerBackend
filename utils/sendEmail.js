d14e5cf1ab09d902602d062d11fdd0a81f04a546
Sanskar-12 <sanskarv2004@gmail.com>
2023-11-13 15:40:17 +0530

    backend

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -4,7 +4,6 @@ const sendEmail = async (to, subject, text) => {
   const transporter = createTransport({
     host: "sandbox.smtp.mailtrap.io",
     port: 2525,
-    secure: false,
     auth: {
       user: "d0262418afb1f6",
       pass: "f734590cf4d3f2"
